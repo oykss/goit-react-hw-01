aa3db893df1e68d680abd6eb24d3647b4378f0ee
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-11-27 23:14:10 +0200

    Added component Profile

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import Profile from "./Profile/Profile";
+import userData from "../data/userData.json";
+
+// import FriendList from "./FriendList/FriendList";
+// import friends from "../data/friends.json";
+
+// import TransactionHistory from "./TransactionHistory/TransactionHistory";
+// import transactions from "../data/transactions.json";
+
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      {/* <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} /> */}
+    </>
+  );
+}
